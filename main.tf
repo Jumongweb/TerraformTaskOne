9c7b9c3c966b606889adc4f67f560bfb62a6faf6
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-12-10 17:11:52 +0100

    refactored main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,30 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_security_group" "existing_web_sg" {
+  filter {
+    name   = "group-name"
+    values = ["web_sg"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-0f989580cd63caf96"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
   key_name      = "terraform-key"
-  security_groups = [aws_security_group.web_sg.name]
+
+  vpc_security_group_ids = [data.aws_security_group.existing_web_sg.id]
 
   tags = {
     Name = "TerraformInstance"
   }
 
+  # Run some initial setup commands (if necessary)
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
@@ -20,39 +34,5 @@ resource "aws_instance" "web" {
               sudo usermod -a -G docker ec2-user
               docker run -d -p 80:80 jumongweb/internship-task-one:latest  
               EOF
-
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Allow HTTP traffic"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-output "web_instance_public_ip" {
-  value = aws_instance.web.public_ip
-}
